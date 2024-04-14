32e2ccf2868adaae38680506c313e4f50c79eb12
Yernar2005 <temirkhanyernar@gmail.com>
2024-04-14 22:13:48 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const cities = "1526384,524901,1526273"; // Almaty, Moscow, Astana
+  const myAPI = "2d736ba9f127f57586b8890b934a839b";
+  const src = `https://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric`;
+
+  const [weatherList, setWeatherList] = useState([]);
+
+  //Set up interceptor, adding API
+  useEffect(() => {
+    const weatherInterceptor = axios.interceptors.request.use(function (
+      config
+    ) {
+      config.params = config.params || {};
+      config.params.appid = myAPI; // checking
+      return config;
+    });
+  }, [myAPI]);
+  //extracting data from website
+  useEffect(() => {
+    axios.get(src).then((response) => {
+      setWeatherList(response.data.list);
+    });
+  }, [src]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {weatherList.map((weather, index) => (
+        <div key={index}>
+          <h3>{weather.name}</h3>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>{weather.weather[0].description}</p>
+        </div>
+      ))}
     </div>
   );
-}
-
-export default App;
+};
